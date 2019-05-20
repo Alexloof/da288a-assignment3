7693cdf99241f1c43fbde1d61495623083665596
Alex Lööf <alex.loofet@gmail.com>
2019-05-20 17:36:41 +0200

    added logic some missing resolvers + views for them

--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -4,110 +4,117 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Store;
+use App\Product;
 
 class StoresController extends Controller
 {
-  public function __construct()
-  {
-    $this->middleware('auth')->except('index', 'show');
-  }
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    $stores = Store::all();
-    return view("stores.index", [
-      'stores' => $stores
-    ]);
-  }
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index', 'show');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $stores = Store::all();
+        return view("stores.index", [
+            'stores' => $stores
+        ]);
+    }
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function create()
-  {
-    return view("stores.create");
-  }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("stores.create");
+    }
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
-    $store = new Store;
-    $store->name = $request->input("name");
-    $store->city = $request->input("city");
-    $store->save();
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $store = new Store;
+        $store->name = $request->input("name");
+        $store->city = $request->input("city");
+        $store->save();
 
-    return redirect()->route('stores.index');
-  }
+        return redirect()->route('stores.index');
+    }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function show($id)
-  {
-    $store = Store::find($id);
-    $store->products = $store->products;
-    return view("stores.show", [
-      'store' => $store
-    ]);
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $store = Store::find($id);
+        return view("stores.show", [
+            'store' => $store
+        ]);
+    }
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function edit($id)
-  {
-    $store = Store::find($id);
-    return view("stores.edit", [
-      'store' => $store
-    ]);
-  }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $store = Store::find($id);
+        $store->productsId = array_column($store->products->toArray(), 'id');
+        $products = Product::all();
+        return view("stores.edit", [
+            'store' => $store,
+            'products' => $products
+        ]);
+    }
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function update(Request $request, $id)
-  {
-    $store = Store::find($id);
-    $store->name = $request->input("name");
-    $store->city = $request->input("city");
-    $store->save();
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $store = Store::find($id);
+        $store->name = $request->input("name");
+        $store->city = $request->input("city");
 
-    return redirect()->route('stores.index');
-  }
+        $store->products()->detach();
+        $products = $request->input("products");
+        $store->products()->attach($products);
+        $store->save();
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy($id)
-  {
-    $store = Store::find($id);
-    $store->products()->detach();
-    Store::destroy($id);
+        return redirect()->route('stores.index');
+    }
 
-    return redirect()->route('stores.index');
-  }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $store = Store::find($id);
+        $store->products()->detach();
+        Store::destroy($id);
+
+        return redirect()->route('stores.index');
+    }
 }
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -8,25 +8,63 @@
         <h4>{{$product->price}} kr</h4>
         <p class="card-text">{{$product->description}}</p>
         <p class="card-text"><small class="text-muted">{{$product->brand}}</small></p>
+        @auth
         <a class="btn btn-primary float-right" href="{{ route('products.edit', ['product' => $product->id]) }}">Ändra produkt</a>
+        @endauth
     </div>
 </div>
-<!-- /.card -->
 
 <div class="card card-outline-secondary my-4">
     <div class="card-header">
-        Product Reviews
+        Finns i butikerna
+    </div>
+    <div class="card-body">
+        @foreach ($product->stores as $store)
+        <div class="d-flex justify-content-between align-items-center">
+            <div>
+                <p>{{$store->name}}</p>
+                <small class="text-muted">{{$store->city}}</small>
+            </div>
+            <div>
+                @auth
+                <a class="btn btn-secondary" href="{{ route('stores.edit', ['store' => $store->id]) }}">Ändra</a>
+                @endauth
+                <a class="btn btn-primary" href="{{ route('stores.show', ['store' => $store->id]) }}">Visa</a>
+            </div>
+        </div>
+
+        <hr>
+        @endforeach
+
+    </div>
+</div>
+
+<div class="card card-outline-secondary my-4">
+    <div class="card-header">
+        Recensioner
     </div>
     <div class="card-body">
         @foreach ($product->reviews as $review)
-        <p>{{$review->comment}}</p>
-        <small class="text-muted">Posted by {{$review->name}}</small>
-        <span class="text-warning">{{str_repeat("★", $review->grade)}}</span>
+        <div class="d-flex justify-content-between align-items-center">
+            <div>
+                <p>{{$review->comment}}</p>
+                <small class="text-muted">Posted by {{$review->name}}</small>
+                <span class="text-warning">{{str_repeat("★", $review->grade)}}</span>
+            </div>
+            <div>
+                @auth
+                <a class="btn btn-secondary" href="{{ route('reviews.edit', ['review' => $review->id]) }}">Ändra</a>
+                @endauth
+                <a class="btn btn-primary" href="{{ route('reviews.show', ['review' => $review->id]) }}">Visa</a>
+            </div>
+        </div>
+
         <hr>
         @endforeach
+        @auth
         <a href="{{ route('reviews.create', ['productId' => $product->id]) }}" class="btn btn-success">Leave a Review</a>
+        @endauth
     </div>
 </div>
-<!-- /.card -->
 
 @endsection
