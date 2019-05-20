e423d434058412665b1b1078092765c07d0d51e1
Alex Lööf <alex.loofet@gmail.com>
2019-05-20 12:28:31 +0200

    added create view for products

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,5 +2,33 @@
 
 @section('content')
 <h1>Skapa Produkt</h1>
-<p>ett formulär här</p>
+<form action="{{ route('products.store') }}" method="POST">
+  @csrf
+  <div class="form-group">
+    <label for="productName">Produktnamn</label>
+    <input id="productName" type="text" name="title" class="form-control" placeholder="Ange produktnamn" required>
+  </div>
+  <div class="form-group">
+    <label for="productDesc">Beskrivning</label>
+    <textarea id="productDesc" name="description" class="form-control" placeholder="Ange produktbeskrivning" required></textarea>
+  </div>
+  <div class="form-group">
+    <label for="productBrand">Märke</label>
+    <input id="productBrand" type="text" name="brand" class="form-control" placeholder="Ange märke" required>
+  </div>
+  <div class="form-group">
+    <label for="productPrice">Pris</label>
+    <input id="productPrice" type="number" name="price" class="form-control" placeholder="Ange pris" required>
+  </div>
+  <div class="form-group">
+    <label for="productImage">Bild URL</label>
+    <input id="productImage" type="text" name="image" class="form-control" placeholder="Ange bild URL" required>
+  </div>
+  @foreach ($stores as $store)
+  <div class="checkbox">
+    <label><input type="checkbox" name="stores[]" value="{{$store->id}}"> {{$store->name}} - {{$store->city}} </label>
+  </div>
+  @endforeach
+  <button type="submit" class="btn btn-primary">Skapa produkt</button>
+</form>
 @endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,22 +2,24 @@
 
 @section('content')
 @foreach ($products as $product)
-<div class="card mb-3" style="max-width: 540px; cursor: pointer; margin: 0 auto;">
-    <div class="row no-gutters">
-        <div class="col-md-4 d-flex align-items-center">
-            <img src="{{$product->image}}" class="card-img" alt="{{$product->title}}">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">{{$product->title}}</h5>
-                <p class="card-text">{{$product->description}}</p>
-                <p class="card-text"><small class="text-muted">{{$product->brand}}</small></p>
-                <p class="card-text">{{$product->price}} kr</p>
-                <a class="btn btn-primary" href="{{ route('products.show', ['products' => $product->id]) }}">Visa</a>
-            </div>
-        </div>
+<div class="card mb-3">
+  <div class="row no-gutters">
+    <div class="col-md-4 d-flex align-items-center">
+      <img src="{{$product->image}}" class="card-img" alt="{{$product->title}}">
     </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">{{$product->title}}</h5>
+        <p class="card-text">{{$product->description}}</p>
+        <p class="card-text"><small class="text-muted">{{$product->brand}}</small></p>
+        <p class="card-text">{{$product->price}} kr</p>
+        <a class="btn btn-primary" href="{{ route('products.show', ['products' => $product->id]) }}">Visa</a>
+      </div>
+    </div>
+  </div>
 </div>
 @endforeach
-
+<div class="py-2">
+  <a class="btn btn-primary" href="{{ route('products.create') }}">Lägg till produkt</a>
+</div>
 @endsection
